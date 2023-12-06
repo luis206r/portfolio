44f219d1be7f40ed455c9e86c4120bf71ef19262
LUIS ROBLEDO <luis.robledo@utec.edu.pe>
2023-12-05 22:28:53 -0500

    avance2 portfolio

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -17,6 +17,7 @@ export const Home = () => {
             <div className={s.container}>
                 <div className={s.grid}>
                     <div className={s.x2}>
+                        
                         <div className={s.card}>
                             <div className={s.info}>
                                 <img className={s.imageProf} src="./zyro-image2.png"/>
@@ -28,13 +29,24 @@ export const Home = () => {
                             </div>
                         
                         </div>
+                       
                     </div>
                     <div className={s.x1}>
                         
-                        <div className={s.card}>
-                            contenido
-                        
+                        <div className={s.card} style={{
+                            backgroundImage:'url("./github.jpg")', 
+                            backgroundSize: 'cover', // ajusta el tamaño de la imagen para cubrir completamente el div
+                            backgroundPosition: 'center',}} onClick={()=>{
+                                window.open('https://www.github.com/luis206r', '_blank')
+                            }}>
+                                
+                           <div className={s.bottomleft}>
+                            
+                             <img src = "./link.png" style={{width:"30px"}}/> 
+                           </div>
+                           
                         </div>
+                        
                     </div>
                     
                 </div>
@@ -42,7 +54,18 @@ export const Home = () => {
                 <div className={s.grid}>
                     <div className={s.x1}>
                         <div className={s.card}>
-                           contenido
+                            <div className={s.text}>
+                                <h2>Programming Laguages and IDE's:</h2>
+                                <list>
+                                    <li>C++</li>
+                                    <li>Python</li>
+                                    <li>Javascript</li>
+                                    <li>Java</li>
+                                    <li>Clion</li>
+                                    <li>Pycharm</li>
+                                    <li>Vscode</li>
+                                </list>
+                            </div>
                         </div>
                     </div>
                     <div className={s.x2}>
@@ -72,7 +95,29 @@ export const Home = () => {
             <div className={s.container}>
                 <div className={s.v}>
                     <div className={s.card}>
-                        contenido
+                    <div className={s.text}>
+                                <h1>Main Skills:</h1>
+                                <h2>🖌️ Front end</h2>
+                                <list>
+                                    <li><b>React environment</b></li>
+                                    <li><b>React libraries</b></li>
+                                    <li><b>Redux</b></li>
+                                    <li><b>Axios</b> and <b>Fetch</b></li>
+                                    <li>UI kits</li>
+                                    <li>Route management</li>
+                                    <li>Many other things</li>
+                                </list>
+                                
+                                <h2>⚙️ Back end</h2>
+                                <list>
+                                    <li>Node JS</li>
+                                    <li>Postgresql</li>
+                                    <li>Sequelize</li>
+                                    <li>Express</li>
+                                    <li>Authentication methods</li>
+                                    <li>And more</li>
+                                </list>
+                                </div>
                     </div>
                 </div>
 
